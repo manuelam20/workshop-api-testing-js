887266fe1c376bb32fdb9d620a153bda1a33790c
Manuela Molina Grisales <manuelamolinagrisales@MacBook-Pro-de-Manuela.local>
2019-03-16 17:59:35 -0500

    Updating tests with query param

--- test/MyFirstApiConsume.test.ts
+++ test/MyFirstApiConsume.test.ts
@@ -4,12 +4,28 @@ import { expect } from 'chai';
 
 describe('First Api tests', () => {
 
-    it('Consume GET Service, typescript', async() => {
-        return request.get('https://httpbin.org/ip').then((response) => {
+   it('Consume GET Service, typescript', async() => {
+       await request.get('https://httpbin.org/ip').then((response) => {
             expect(response.status).to.equal(200);
             expect(response.body).to.have.property('origin');
         });
 
+    });
+
+    it('Consume GET service with query params', () => {
+        const query = {
+            name: 'Man',
+            age: '31',
+            city: 'New York'
+        };
+        
+        return request.get('https://httpbin.org/get')
+        .query(query)
+        .then((response) => {
+            expect(response.status).to.equal(200);
+            expect(response.body.args).to.eql(query);
+            expect(response.header).to.have.property('access-control-allow-credentials')
+        });
     });
 
 
